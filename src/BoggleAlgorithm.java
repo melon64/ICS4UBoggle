84906c50d4b919f4941a20cd7d2830bd5a6792ff
340943950 <340943950@gapps.yrdsb.ca>
2022-06-07 17:53:35 -0400

    Added a recursive method to check if a word can be formed from a given position and a method to clone 2D char arrays

--- src/BoggleAlgorithm.java
+++ src/BoggleAlgorithm.java
@@ -9,6 +9,45 @@ package src;
 import java.util.ArrayList;
 
 public class BoggleAlgorithm {
+    public static boolean canFormWordFromPos(char[][] board, String word, int currRow, int currCol) {
+        if (word.length() == 0) {
+            return true;
+        } else {
+            ArrayList<Integer[]> moveList = getMoveList(board, currRow, currCol, word.charAt(0));
+            for (Integer[] move: moveList) {
+                // Create a copy of word called newWord and remove the first letter of 
+                // newWord as this letter was just found
+                String newWord = word.substring(1, word.length());
+                
+                // Create a copy of board called newBoard so that any edits made on 
+                // newBoard do not change the original
+                char[][] newBoard = clone2DCharArr(board);
+                // Remove the letter that was just used to form part of the word from 
+                // the board so that it isn't reused (as this would be illegal)
+                newBoard[currRow][currCol] = ' ';
+                
+                // Make a recursive call to check if the rest of the word can be formed
+                if (canFormWordFromPos(newBoard, newWord, currRow + move[0], currCol + move[1])) {
+                    return true;
+                }
+            }
+            
+            // If the program didn't find the word on the board from the starting 
+            // position yet, return false
+            return false;
+        }
+    }
+
+    public static char[][] clone2DCharArr(char[][] arr) {
+        char[][] newArr = new char[arr.length][arr[0].length];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                newArr[i][j] = arr[i][j];
+            }
+        }
+        return newArr;
+    }
+    
     public static ArrayList<Integer[]> getMoveList(char[][] board, int currRow, int currCol, char letterToFind) {
         ArrayList<Integer[]> moveList = new ArrayList<Integer[]>();
         
