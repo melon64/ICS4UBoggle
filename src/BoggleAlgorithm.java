ba52bee0451c705bac8f32bfe1847f68487cb661
340943950 <340943950@gapps.yrdsb.ca>
2022-06-07 17:49:54 -0400

    Created a java class which will contain all Boggle algorithms

---
+++ src/BoggleAlgorithm.java
@@ -0,0 +1,11 @@
+package src;
+
+/**
+ * Names: Adarsh P, Larris X, Felix X, and Hubert X
+ * Date: June 7, 2022
+ * Description: A program that contains a number of useful algorithms for the game of Boggle
+ */
+
+public class BoggleAlgorithm {
+    
+}
