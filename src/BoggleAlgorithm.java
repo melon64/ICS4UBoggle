efc636482d76abaa2091d783f1c3e017d1d6f596
340943950 <340943950@gapps.yrdsb.ca>
2022-06-07 17:51:44 -0400

    Added a method to get a list of all valid adjacent positions from a given cell

--- src/BoggleAlgorithm.java
+++ src/BoggleAlgorithm.java
@@ -6,6 +6,35 @@ package src;
  * Description: A program that contains a number of useful algorithms for the game of Boggle
  */
 
+import java.util.ArrayList;
+
 public class BoggleAlgorithm {
-    
+    public static ArrayList<Integer[]> getMoveList(char[][] board, int currRow, int currCol, char letterToFind) {
+        ArrayList<Integer[]> moveList = new ArrayList<Integer[]>();
+        
+        for (int rowMove = -1; rowMove <= 1; rowMove++) {
+            for (int colMove = -1; colMove <= 1; colMove++) {
+                if (rowMove == 0 && colMove == 0) {
+                    // You cannot use the letter in the same spot again - ensure that the 
+                    // next letter is found in a different position than the current letter 
+                    // - if not, ignore this move
+                    continue;
+                } else if (currRow + rowMove < 0 || currRow + rowMove >= board.length) {
+                    // Ensure that the cell after the move is in a valid row (a row that 
+                    // actually exists on the board) - if not, ignore this move
+                    continue;
+                } else if (currCol + colMove < 0 || currCol + colMove >= board[0].length) {
+                    // Ensure that the cell after the move is in a valid row (a row that 
+                    // actually exists on the board) - if not, ignore this move
+                    continue;
+                } else if (board[currRow + rowMove][currCol + colMove] == letterToFind) {
+                    // If the cell that is being moved to contains the letter that needs to 
+                    // be found, add this move to the list of valid moves
+                    moveList.add(new Integer[]{rowMove, colMove});
+                }
+            }
+        }
+
+        return moveList;
+    }
 }
