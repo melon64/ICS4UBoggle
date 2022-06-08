f0315efd90854dbb57ac278a879b5edc3a4586e6
340943950 <340943950@gapps.yrdsb.ca>
2022-06-08 12:45:39 -0400

    Created a java file to initialize the setup menu GUI

---
+++ src/BoggleGame.java
@@ -0,0 +1,13 @@
+package ICS4UBoggle.src;
+
+/**
+ * Names: Adarsh P, Larris X, Felix X, and Hubert X
+ * Date: June 7, 2022
+ * Description: A program that contains the game loop and initializes the GUIs
+ */
+
+public class BoggleGame {
+    public static void main(String[] args) {
+        BoggleSetupMenu menuGUI = new BoggleSetupMenu();
+    }
+}
